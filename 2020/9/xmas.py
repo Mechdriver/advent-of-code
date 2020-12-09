b9268009be51385f9d175d17c0fb3a848eb31a01
Zach Behnke <ztbehnke62@gmail.com>
2020-12-08 22:15:22 -0800

    Day 9 - Part 2

--- 2020/9/xmas.py
+++ 2020/9/xmas.py
@@ -1,10 +1,27 @@
 WINDOW_SIZE = 25
 
-def find_bad_number():
+def crack_encryption():
     with open("preamble.txt", "r", encoding="utf-8") as input_file:
         nums = list(map(int, input_file.readlines()))
 
+    cracked = False
+    contiguous_set_len = 2
+    contiguous_set = []
+    bad_num = _find_bad_number(nums)
+    search_nums = nums[:nums.index(bad_num)]
 
+    while not cracked:
+        for ndx in range(0, len(search_nums) - (contiguous_set_len - 1)):
+            contiguous_set = search_nums[ndx:ndx + contiguous_set_len]
+            if sum(contiguous_set) == bad_num:
+                cracked = True
+                break
+
+        contiguous_set_len += 1
+    return min(contiguous_set) + max(contiguous_set)
+
+
+def _find_bad_number(nums):
     for ndx in range(0, len(nums) - WINDOW_SIZE):
         done = True
         target_num = nums[ndx + WINDOW_SIZE]
@@ -19,4 +36,4 @@ def find_bad_number():
             return target_num
 
 
-print(find_bad_number())
+print(crack_encryption())
