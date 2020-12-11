bbfbe291124965a4c633ed7d46a9a35a164ddcec
Zach Behnke <ztbehnke62@gmail.com>
2020-12-10 22:31:04 -0800

    Recursive refactor

--- 2020/11/seat_planning.py
+++ 2020/11/seat_planning.py
@@ -1,18 +1,59 @@
 from pprint import pprint
 from copy import deepcopy
+from enum import Enum
 
 FLOOR = '.'
 EMPTY = 'L'
 FULL = '#'
 
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+    UP_LEFT = 5
+    UP_RIGHT = 6
+    DOWN_LEFT = 7
+    DOWN_RIGHT = 9
 
-def find_empty_seats():
+
+def _check_seats(layout, direction, y, x):
+    # Check left
+    if direction == Direction.LEFT and x >= 0 and layout[y][x] == FULL:
+        #print(f'{x} {y}')
+        return 1
+    # Check right
+    if direction == Direction.RIGHT and x < len(layout[y]) and layout[y][x] == FULL:
+        return 1
+    # Check up
+    if direction == Direction.UP and y >= 0 and layout[y][x] == FULL:
+        return 1
+    # Check down
+    if direction == Direction.DOWN and y < len(layout) and layout[y][x] == FULL:
+        return 1
+    # Check up left
+    if direction == Direction.UP_LEFT and y >= 0 and x >= 0 and layout[y][x] == FULL:
+        return 1
+    # Check up right
+    if direction == Direction.UP_RIGHT and y >= 0 and x < len(layout[y]) and layout[y][x] == FULL:
+        return 1
+    # Check down left
+    if direction == Direction.DOWN_LEFT and y < len(layout) and x >= 0 and layout[y][x] == FULL:
+        return 1
+    # Check down right
+    if direction == Direction.DOWN_RIGHT and y < len(layout)and x < len(layout[y]) and layout[y][x] == FULL:
+        return 1
+    return 0
+
+
+def find_occupied_seats():
     with open("seat_layout.txt", "r", encoding="utf-8") as input_file:
         next_layout = list(map(lambda line: list(line.strip()), input_file.readlines()))
 
     state_changed = True
 
     while state_changed:
+    #for i in range(0, 2):
         state_changed = False
         layout = deepcopy(next_layout)
 
@@ -21,30 +62,21 @@ def find_empty_seats():
                 adj_seats = 0
 
                 # Check left
-                if x > 0 and row[x - 1] == FULL:
-                    adj_seats += 1
+                adj_seats += _check_seats(layout, Direction.LEFT, y, x - 1)
                 # Check right
-                if x < len(row) - 1 and row[x + 1] == FULL:
-                    adj_seats += 1
+                adj_seats += _check_seats(layout, Direction.RIGHT, y, x + 1)
                 # Check up
-                if y > 0 and layout[y - 1][x] == FULL:
-                    adj_seats += 1
+                adj_seats += _check_seats(layout, Direction.UP, y - 1, x)
                 # Check down
-                if y < len(layout) - 1 and layout[y + 1][x] == FULL:
-                    adj_seats += 1
-
+                adj_seats += _check_seats(layout, Direction.DOWN, y + 1, x)
                 # Check up left
-                if y > 0 and x > 0 and layout[y - 1][x - 1] == FULL:
-                    adj_seats += 1
+                adj_seats += _check_seats(layout, Direction.UP_LEFT, y - 1, x - 1)
                 # Check up right
-                if y > 0 and x < len(row) - 1 and layout[y - 1][x + 1] == FULL:
-                    adj_seats += 1
+                adj_seats += _check_seats(layout, Direction.UP_RIGHT, y - 1, x + 1)
                 # Check down left
-                if y < len(layout) - 1 and x > 0 and layout[y + 1][x - 1] == FULL:
-                    adj_seats += 1
+                adj_seats += _check_seats(layout, Direction.DOWN_LEFT, y + 1, x - 1)
                 # Check down right
-                if y < len(layout) - 1 and x < len(row) - 1 and layout[y + 1][x + 1] == FULL:
-                    adj_seats += 1
+                adj_seats += _check_seats(layout, Direction.DOWN_RIGHT, y + 1, x + 1)
 
                 if space == EMPTY and adj_seats == 0:
                     next_layout[y][x] = FULL
@@ -52,15 +84,18 @@ def find_empty_seats():
                 elif space == FULL and adj_seats >= 4:
                     next_layout[y][x] = EMPTY
                     state_changed = True
+        #pprint(next_layout)
+        #print()
+        #print()
 
     occupied_seats = 0
-
     for row in layout:
         for space in row:
             if space == FULL:
                 occupied_seats += 1
 
     print(occupied_seats)
+    #pprint(next_layout)
 
 
-find_empty_seats()
+find_occupied_seats()
