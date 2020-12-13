dddd7c8534b8a90a558688fe796d590bc4dcc3d6
Zach Behnke <ztbehnke62@gmail.com>
2020-12-12 22:39:56 -0800

    Day 13 - Part. Haha I did a library...

--- 2020/13/shuttle_search.py
+++ 2020/13/shuttle_search.py
@@ -1,3 +1,38 @@
+from sympy.ntheory.modular import crt
+
+
+def find_seq_bus_times_lazy():
+    with open("bus_times.txt", "r", encoding="utf-8") as input_file:
+        shuttle_data = list(map(lambda line: line.strip(), input_file.readlines()))
+
+    bus_ids = [int(id) if id.isnumeric() else id for id in shuttle_data[1].split(',')]
+    ids = [id for id in bus_ids if id != 'x']
+    remainders = [id - offset for offset, id in enumerate(bus_ids) if id != 'x']
+    print(crt(ids, remainders)[0])
+
+
+def find_seq_bus_times_brute():
+    with open("bus_times.txt", "r", encoding="utf-8") as input_file:
+        shuttle_data = list(map(lambda line: line.strip(), input_file.readlines()))
+
+    bus_ids = [int(id) if id.isnumeric() else id for id in shuttle_data[1].split(',')]
+    id_to_offset = {id: ndx for ndx, id in enumerate(bus_ids) if id != 'x'}
+    done = False
+    timestamp = 0
+
+    while not done:
+        timestamp += bus_ids[0]
+        done = True
+
+        for id, offset in id_to_offset.items():
+            if (timestamp + offset) % id:
+                done = False
+                break
+
+    print(timestamp)
+
+
+
 
 def find_shuttle():
     with open("bus_times.txt", "r", encoding="utf-8") as input_file:
@@ -18,4 +53,5 @@ def find_shuttle():
 
 
 
-find_shuttle()
+find_seq_bus_times_lazy()
+#find_seq_bus_times_brute()
