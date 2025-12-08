8d2cd6b925525b129afe1d78a2d27047960b9ae8
Zach Behnke <zach.behnke@ezcater.com>
2025-12-08 11:44:19 -0500

    Day 7 Part 2 Smart

--- 2025/laboratories/laboratories.go
+++ 2025/laboratories/laboratories.go
@@ -6,6 +6,11 @@ import (
 	"strings"
 )
 
+type Point struct {
+	display string
+	paths   int
+}
+
 func getSplits(manifoldGrid [][]string) int {
 	splitCount := 0
 
@@ -82,6 +87,87 @@ func getTimelines(manifoldGrid [][]string, i int) int {
 	return timeLines
 }
 
+func printManifoldPoints(manifoldPoints [][]Point) {
+	for _, points := range manifoldPoints {
+		var displayList []string
+		var pathsList []int
+		for _, point := range points {
+			displayList = append(displayList, point.display)
+			pathsList = append(pathsList, point.paths)
+		}
+		fmt.Println(displayList, pathsList)
+	}
+	fmt.Println()
+}
+
+func buildSplits(manifoldPoints [][]Point, verbose bool) [][]Point {
+	for i := 1; i < len(manifoldPoints); i++ {
+		for j := 0; j < len(manifoldPoints[i]); j++ {
+
+			if manifoldPoints[i][j].display == "." {
+				if manifoldPoints[i-1][j].display == "S" || manifoldPoints[i-1][j].display == "|" {
+					manifoldPoints[i][j].display = "|"
+				}
+			}
+
+			if manifoldPoints[i][j].display == "^" && manifoldPoints[i-1][j].display == "|" {
+				if j > 0 {
+					manifoldPoints[i][j-1].display = "|"
+				}
+
+				if j < len(manifoldPoints[i]) {
+					manifoldPoints[i][j+1].display = "|"
+				}
+			}
+		}
+		if verbose {
+			printManifoldPoints(manifoldPoints)
+		}
+	}
+
+	return manifoldPoints
+}
+
+func buildPaths(manifoldPoints [][]Point, verbose bool) [][]Point {
+	for i := 1; i < len(manifoldPoints); i++ {
+		for j := 0; j < len(manifoldPoints[i]); j++ {
+
+			if manifoldPoints[i][j].display == "|" {
+				switch manifoldPoints[i-1][j].display {
+				case "|":
+					manifoldPoints[i][j].paths = manifoldPoints[i-1][j].paths
+				case "S":
+					manifoldPoints[i][j].paths = 1
+				}
+
+				if j < len(manifoldPoints[i])-1 && manifoldPoints[i][j+1].display == "^" {
+					manifoldPoints[i][j].paths += manifoldPoints[i-1][j+1].paths
+				}
+
+				if j > 0 && manifoldPoints[i][j-1].display == "^" {
+					manifoldPoints[i][j].paths += manifoldPoints[i-1][j-1].paths
+				}
+			}
+		}
+		if verbose {
+			printManifoldPoints(manifoldPoints)
+		}
+	}
+	return manifoldPoints
+}
+
+func getTimelinesSmart(manifoldPoints [][]Point) int {
+	timelines := 0
+
+	builtManifold := buildPaths(buildSplits(manifoldPoints, false), false)
+
+	for _, point := range builtManifold[len(builtManifold)-1] {
+		timelines += point.paths
+	}
+
+	return timelines
+}
+
 func Part1() int {
 	buffer, err := os.ReadFile("./inputs/day7/example_input.txt")
 
@@ -106,7 +192,7 @@ func Part1() int {
 }
 
 func Part2() int {
-	buffer, err := os.ReadFile("./inputs/day7/example_input.txt")
+	buffer, err := os.ReadFile("./inputs/day7/test_input.txt")
 
 	if err != nil {
 		panic(err)
@@ -119,11 +205,15 @@ func Part2() int {
 		lines = lines[:len(lines)-1]
 	}
 
-	var manifoldGrid [][]string
+	var manifoldPoints [][]Point
 
 	for i := 0; i < len(lines)-1; i++ {
-		manifoldGrid = append(manifoldGrid, strings.Split(lines[i], ""))
+		var points []Point
+		for _, item := range strings.Split(lines[i], "") {
+			points = append(points, Point{item, 0})
+		}
+		manifoldPoints = append(manifoldPoints, points)
 	}
 
-	return getTimelines(manifoldGrid, 1)
+	return getTimelinesSmart(manifoldPoints)
 }
